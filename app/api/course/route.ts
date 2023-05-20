fddc6bcb2bf598b4bfc3e09c204c30fac8bec190
tiagocarleet <133457029+tiagocarleet@users.noreply.github.com>
2023-05-20 00:12:04 +0000

    Metodo GET

---
+++ app/api/course/route.ts
@@ -0,0 +1,11 @@
+import { getAllCourse } from "/app/model/CourseService"
+import { NextResponse } from "next/server"
+
+export async function GET(){
+const courses = await getAllCourse()
+console.log("Acessou GET...")
+return NextResponse.json(courses)
+
+}
+
+
