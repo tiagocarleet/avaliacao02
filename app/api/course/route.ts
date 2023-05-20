36eff2bc434e0a931efdc87eacfe13b9839b4fcf
tiagocarleet <133457029+tiagocarleet@users.noreply.github.com>
2023-05-20 00:25:04 +0000

    Metodo GET

--- app/api/course/route.ts
+++ app/api/course/route.ts
@@ -2,9 +2,9 @@ import { getAllCourse } from "@/app/model/CourseService"
 import { NextResponse } from "next/server"
 
 export async function GET(){
-const courses = await getAllCourse()
-console.log("Acessou GET...")
-return NextResponse.json(courses)
+    const courses = await getAllCourse()
+    console.log("Acessou GET...")
+    return NextResponse.json(courses)
 
 }
 
--- app/model/CourseService.ts
+++ app/model/CourseService.ts
@@ -1,6 +1,6 @@
 import { sql } from "@vercel/postgres";
 
 export async function getAllCourse(){
-    const { rows:students } = await sql`SELECT * from course;`;
-    return students
+    const { rows:courses } = await sql`SELECT * from course;`;
+    return courses
 }
