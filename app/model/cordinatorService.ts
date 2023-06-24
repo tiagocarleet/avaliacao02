69e44f42fe7ac6371f2729c8e6eb821ae97107cf
tiagocarleet <133457029+tiagocarleet@users.noreply.github.com>
2023-06-24 00:27:05 +0000

    agora vai

--- app/model/cordinatorService.ts
+++ app/model/cordinatorService.ts
@@ -1,13 +1,13 @@
 import { sql } from "@vercel/postgres";
 
 export async function getAllCordinator(){
-    const { rows:cordinator } = await sql`SELECT * from cordinators;`;
+    const { rows:cordinator } = await sql `SELECT * from cordinators;`;
     return cordinator
 }
 
 export async function addCordinator(name:string, email:string){
     console.log("Entrou da add", name, email)
-    await sql`INSERT INTO cordinators values(${name}, ${email});`
+    await sql `INSERT INTO cordinators values(${name}, ${email});`
 }
 
 export async function deleteCordinatorById(id:string){
